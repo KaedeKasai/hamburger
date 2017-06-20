d064d315ee6c02dee8f01f59d1e60b56b5f74e52
KaedeKasai <kimaguret@icloud.com>
2017-06-20 16:09:02 +0900

    mainを整理（2）

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		
 		makeHamburger();
-		
 		System.out.println("\n---------------------------------------\n");
 		makeHealthyHamburger();
 		System.out.println("\n---------------------------------------\n");
@@ -15,17 +14,21 @@ public class Main {
 	
 	public static void makeHamburger() {
 	
-	Hamburger hamburger = new Hamburger("ベーシック", "ソーセージ", 350, "レギュラー");
+		Hamburger hamburger = new Hamburger("ベーシック", "ソーセージ", 350, "レギュラー");
+		
 		hamburger.baseBurger();
 		hamburger.addTopping("トマト", 30);
 		hamburger.addTopping("レタス", 75);
 		hamburger.addTopping("チーズ", 100);
 		hamburger.itemizeBurger();
 		hamburger.showBurgersTopping();
+		
 	}
 	
 	public static void makeHealthyHamburger() {
+		
 		HealthyBurger healthyHamburger = new HealthyBurger("ヘルシー", "ベーコン", 500, "ライ麦");
+		
 		healthyHamburger.baseBurger();
 		healthyHamburger.addTopping("トマト", 30);
 		healthyHamburger.addTopping("レタス", 75);
@@ -39,7 +42,9 @@ public class Main {
 	}
 	
 	public static void makeDeluxeBurger() {
+		
 		DeluxeBurger deluxeBurger = new DeluxeBurger("デラックス", "ベーコン＆ソーセージ", 1000, "バンズ＆クラブ");
+		
 		deluxeBurger.baseBurger();
 		deluxeBurger.showBurgersTopping();
 	}
